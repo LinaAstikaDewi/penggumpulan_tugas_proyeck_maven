fe6795ba312a60d6c260ca989a044c23212d51b8
baiqwulan2002@gmail.com <linaastika99@gmail.com>
2022-12-15 11:10:47 +0800

    first commit

---
+++ src/main/java/Lina/astika/dewi/maven/App.java
@@ -0,0 +1,13 @@
+package Lina.astika.dewi.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
